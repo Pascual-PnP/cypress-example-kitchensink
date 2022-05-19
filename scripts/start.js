7bfe116119154d2cccf8e34c2a7f9bbfec120bff
Pascual <p.queralt@pnpt.com>
2022-05-19 15:38:44 +0200

    console.log

--- scripts/start.js
+++ scripts/start.js
@@ -8,7 +8,7 @@ if (process.env.CI) cmd += ` --no-clipboard`
 if (process.argv.length > 2) cmd += ` ${process.argv.slice(2).join(' ')}`
 
 // eslint-disable-next-line no-console
-console.log(`Running "${cmd}"...`)
+console.log(`Running Pascualete run run"${cmd}"...`)
 // Please delete this comment
 // and this one as well
 execSync(cmd, { stdio: 'inherit' })
